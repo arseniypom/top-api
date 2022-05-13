578b44dac9e0d63b30afc8707d3db202810563bc
Arseniy Pomazkov <arpomazkov@bk.ru>
2022-05-13 08:50:53 +0300

    Added models

---
+++ src/product/product.model.ts
@@ -0,0 +1,16 @@
+export class ProductModel {
+  img: string;
+  title: string;
+  price: number;
+  oldPrice: number;
+  credit: number;
+  calculateRating: number;
+  description: string;
+  advantages: string;
+  disadvantages: string;
+  categories: string[];
+  tags: string;
+  charachteristics: {
+    [key: string]: string;
+  };
+}
