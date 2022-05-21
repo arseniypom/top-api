7970d15b476835e715980a87f3fe13cc8380c72f
Arseniy Pomazkov <arpomazkov@bk.ru>
2022-05-21 08:21:30 +0300

    Added typegoose schema decorators

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,49 @@
-export class ProductModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+  @prop()
+  name: string;
+
+  @prop()
+  value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @prop()
   img: string;
+
+  @prop()
   title: string;
+
+  @prop()
   price: number;
+
+  @prop()
   oldPrice: number;
+
+  @prop()
   credit: number;
+
+  @prop()
   calculateRating: number;
+
+  @prop()
   description: string;
+
+  @prop()
   advantages: string;
+
+  @prop()
   disadvantages: string;
+
+  @prop({ type: () => [String] })
   categories: string[];
-  tags: string;
-  charachteristics: {
-    [key: string]: string;
-  };
+
+  @prop({ type: () => [String] })
+  tags: string[];
+
+  @prop({ type: () => [ProductCharacteristic], _id: false })
+  charachteristics: ProductCharacteristic[];
 }
